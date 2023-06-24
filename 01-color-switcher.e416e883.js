b67875eb90b32c2c429568f6477650f6265e7c93
AnastasiaCarlii <124528864+AnastasiaCarlii@users.noreply.github.com>
2023-06-24 21:40:45 +0000

    Deploying to gh-pages from @ AnastasiaCarlii/goit-js-hw-09@630a968bc9272e5dc02b635100550f90d6c09a81 🚀

---
+++ 01-color-switcher.e416e883.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),r=null;function o(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.addEventListener("click",(function(){e.setAttribute("disabled",!0),r=setInterval(o,1e3)})),n.addEventListener("click",(function(){e.removeAttribute("disabled"),clearInterval(r)}))}();
+//# sourceMappingURL=01-color-switcher.e416e883.js.map
