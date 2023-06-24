630a968bc9272e5dc02b635100550f90d6c09a81
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-06-24 22:39:52 +0100

    first

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId = null;
+
+function changeBackgroundColor() {
+  body.style.backgroundColor = getRandomHexColor();
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+startBtn.addEventListener('click', () => {
+  startBtn.setAttribute('disabled', true);
+  intervalId = setInterval(changeBackgroundColor, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  startBtn.removeAttribute('disabled');
+  clearInterval(intervalId);
+});
